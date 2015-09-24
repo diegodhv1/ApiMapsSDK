60e3d941c1f0ec4a7dfc2eba688c956224d2a8d5
Diego <diegodhv1@gmail.com>
2015-09-24 10:48:52 -0500

    test maps
    test maps with plugin phonhegap

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.1.0",
+    "plugin.google.maps": "1.3.1"
+}
+// BOTTOM OF METADATA
+});
